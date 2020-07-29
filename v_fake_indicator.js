ce5f1f42b8ea0e497d17231b7d430d11bb239734
TickInterval <63548293+tickinterval@users.noreply.github.com>
2020-07-29 19:15:28 +0300

    Create v_fake_indicator.js

---
+++ v_fake_indicator.js
@@ -0,0 +1,272 @@
+//Tickbase
+function get_icon(a) {
+    var letter = ""
+    switch (a) {
+        case "desert eagle":
+            letter = "a"
+            break
+        case "dual berettas":
+            letter = "b"
+            break
+        case "five seven":
+            letter = "c"
+            break
+        case "glock 18":
+            letter = "d"
+            break
+        case "ak 47":
+            letter = "e"
+            break
+        case "aug":
+            letter = "f"
+            break
+        case "awp":
+            letter = "g"
+            break
+        case "famas":
+            letter = "h"
+            break
+        case "m249":
+            letter = "i"
+            break
+        case "g3sg1":
+            letter = "j"
+            break
+        case "galil ar":
+            letter = "k"
+            break
+        case "m4a4":
+            letter = "l"
+            break
+        case "m4a1 s":
+            letter = "m"
+            break
+        case "mac 10":
+            letter = "n"
+            break
+        case "p2000":
+            letter = "o"
+            break
+        case "mp5 sd":
+            letter = "p"
+            break
+        case "ump 45":
+            letter = "q"
+            break
+        case "xm1014":
+            letter = "r"
+            break
+        case "pp bizon":
+            letter = "s"
+            break
+        case "mag 7":
+            letter = "t"
+            break
+        case "negev":
+            letter = "u"
+            break
+        case "sawed off":
+            letter = "v"
+            break
+        case "tec 9":
+            letter = "w"
+            break
+        case "zeus x27":
+            letter = "x"
+            break
+        case "p250":
+            letter = "y"
+            break
+        case "mp7":
+            letter = "z"
+            break
+        case "mp9":
+            letter = "A"
+            break
+        case "nova":
+            letter = "B"
+            break
+        case "p90":
+            letter = "C"
+            break
+        case "scar 20":
+            letter = "D"
+            break
+        case "sg 553":
+            letter = "E"
+            break
+        case "ssg 08":
+            letter = "F"
+            break
+        case "knife":
+            letter = "G"
+            break
+        case "flashbang":
+            letter = "H"
+            break
+        case "high explosive grenade":
+            letter = "I"
+            break
+        case "smoke grenade":
+            letter = "J"
+            break
+        case "molotov":
+            letter = "K"
+            break
+        case "decoy grenade":
+            letter = "L"
+            break
+        case "incendiary grenade":
+            letter = "M"
+            break
+        case "c4 explosive":
+            letter = "N"
+            break
+        case "usp s":
+            letter = "P"
+            break
+        case "cz75 auto":
+            letter = "Q"
+            break
+        case "r8 revolver":
+            letter = "R"
+            break
+        case "bayonet":
+            letter = "V"
+            break
+        case "flip knife":
+            letter = "W"
+            break
+        case "gut knife":
+            letter = "X"
+            break
+        case "karambit":
+            letter = "Y"
+            break
+        case "m9 bayonet":
+            letter = "Z"
+            break
+        case "falchion knife":
+            letter = "1"
+            break
+        case "bowie knife":
+            letter = "2"
+            break
+        case "butterfly knife":
+            letter = "3"
+            break
+        case "shadow daggers":
+            letter = "4"
+            break
+        case "ursus knife":
+            letter = "5"
+            break
+        case "navaja knife":
+            letter = "6"
+            break
+        case "stiletto knife":
+            letter = "7"
+            break
+        case "skeleton knife":
+            letter = "8"
+            break
+        case "huntsman knife":
+            letter = "0"
+            break
+        case "talon knife":
+            letter = "8"
+            break
+        case "classic knife":
+            letter = "25"
+            break
+        case "paracord knife":
+            letter = "Z"
+            break
+        case "survival knife":
+            letter = "Z"
+            break
+        case "nomad knife":
+            letter = "Z"
+            break
+        default:
+            letter = ""
+            break
+    }
+    return letter
+}
+
+UI.AddLabel("                   Tickbase            ");
+UI.AddSliderInt("Tickbase_x", 0, Global.GetScreenSize()[0]);
+UI.AddSliderInt("Tickbase_y", 0, Global.GetScreenSize()[1]);
+
+function in_bounds(vec, x, y, x2, y2) {
+    return (vec[0] > x) && (vec[1] > y) && (vec[0] < x2) && (vec[1] < y2)
+}
+var fa = 0;
+var sa = 0;
+
+function main_dt() {
+    if (!World.GetServerString()) return;
+
+    const x = UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Tickbase_x"),
+        y = UI.GetValue("Misc", "JAVASCRIPT", "Script items", "Tickbase_y");
+
+    localplayer_index = Entity.GetLocalPlayer();
+    localplayer_weapon = Entity.GetWeapon(localplayer_index);
+    weapon_name = Entity.GetName(localplayer_weapon);
+    g_Local_classname = Entity.GetClassName(localplayer_weapon);
+    var nextattack = Entity.GetProp(localplayer_weapon, "CBaseCombatWeapon", "m_flNextPrimaryAttack");
+    var CanShoot = false;
+    if (nextattack <= Globals.Curtime()) {
+        CanShoot = true;
+    }
+
+    var frames = 8 * Globals.Frametime();
+
+    var font = Render.AddFont("Verdana", 7, 100);
+    var fontbullet = Render.AddFont("bullet", 18, 100);
+    if (CanShoot && Exploit.GetCharge() == 1 && UI.IsHotkeyActive("Rage", "Exploits", "Doubletap")) {
+        var text = "DT [v2.8 debug] | tickbase(v): 16";
+        var color = [89, 119, 239, 255];
+    } else if (CanShoot && Exploit.GetCharge() == 1 && UI.IsHotkeyActive("Rage", "Exploits", "Hide shots")) {
+        var text = "DT [v2.8 debug] | tickbase(v): 7";
+        var color = [89, 119, 239, 255];
+    } else {
+        var text = "DT [v2.8 debug] | tickbase(v): 0";
+        var color = [89, 89, 89, 255];
+    }
+    var w = Render.TextSizeCustom(text, font)[0] + 8;
+
+    Render.FilledRect(x, y, w, 2, color);
+    Render.FilledRect(x, y + 2, w, 18, [17, 17, 17, 255]);
+    Render.StringCustom(x + 5, y + 5, 0, text, [0, 0, 0, 180], font);
+    Render.StringCustom(x + 4, y + 4, 0, text, [255, 255, 255, 255], font);
+
+    Render.String(x + 4, y + 22, 0, get_icon(weapon_name), [255, 255, 255, 255], 5);
+    if ((g_Local_classname == "CKnife" || g_Local_classname == "CWeaponSSG08" || g_Local_classname == "CWeaponAWP" || weapon_name == "r8 revolver" || g_Local_classname == "CHEGrenade" || g_Local_classname == "CMolotovGrenade" || g_Local_classname == "CIncendiaryGrenade" || g_Local_classname == "CFlashbang" || g_Local_classname == "CSmokeGrenade" || g_Local_classname == "CDecoyGrenade" || g_Local_classname == "CWeaponTaser" || g_Local_classname == "CC4")) {
+        //return
+    } else {
+        if (CanShoot) {
+            fa = Math.min(fa + frames, 1);
+            Render.StringCustom(x + 10 + Render.TextSize(get_icon(weapon_name), 5)[0], y + 18, 0, "A", [255, 255, 255, fa * 255], fontbullet);
+        } else {
+            fa = 0;
+        }
+        if (CanShoot && Exploit.GetCharge() == 1 && UI.IsHotkeyActive("Rage", "Exploits", "Doubletap")) {
+            sa = Math.min(sa + frames, 1);
+            Render.StringCustom(x + 30 + Render.TextSize(get_icon(weapon_name), 5)[0], y + 18, 0, "A", [255, 255, 255, sa * 255], fontbullet);
+        } else {
+            sa = 0;
+        }
+    }
+
+
+    if (Global.IsKeyPressed(1) && UI.IsMenuOpen()) {
+        const mouse_pos = Global.GetCursorPosition();
+        if (in_bounds(mouse_pos, x, y, x + w, y + 30)) {
+            UI.SetValue("Misc", "JAVASCRIPT", "Script items", "Tickbase_x", mouse_pos[0] - w / 2);
+            UI.SetValue("Misc", "JAVASCRIPT", "Script items", "Tickbase_y", mouse_pos[1] - 20);
+        }
+    }
+}
+Global.RegisterCallback("Draw", "main_dt");
